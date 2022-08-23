c93fb6aee0a5b8338758b2dc8caafaad20eb6c58
hagar <hagar20191700724@cis.asu.edu.eg>
2022-08-23 23:58:22 +0200

    finished--/

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -23,8 +23,6 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
-    }
+
     }
 
